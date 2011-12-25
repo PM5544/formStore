d7c5fd709098bc03253157ebfac97255225d2a51
Joël Kuijten <joel@pm5544.eu>
2011-12-25 13:26:33 +0100

    fixed the id of the container which caused the styling not being applied

--- restore.js
+++ restore.js
@@ -223,12 +223,12 @@
 
         }
 
-        addStyleRule( "#pm5544StoreContainer", "text-align:left; position:fixed; z-index:9999999; width:500px; background:#fff; border:1px solid #eee; border-radius:5px; padding:5px; left:50%; top:10%; font:normal 12px/30px Verdana,sans-serif; margin-left:-250px; min-height:60px; max-height:80%; overflow-y:auto; box-shadow:2px 3px 10px rgba(0,0,0,.4);" );
-        addStyleRule( "#pm5544StoreContainer a", "text-align:left; color:#000; display:inline-block; line-height:20px; padding:0 5px; width:445px; cursor:pointer; border:1px solid #e7e7e7; border-radius:5px; text-decoration:none;");
-        addStyleRule( "#pm5544StoreContainer a.select:hover", "background:#e7e7e7;" );
-        addStyleRule( "#pm5544StoreContainer a.select:hover:before", "content:'fill form with: ';" );
-        addStyleRule( "#pm5544StoreContainer a.remove", "text-align:center; color:#8e1111; font-weight:bold; border:1px solid #8e1111; line-height:18px; margin-left:5px; width:19px; padding:0; cursor:pointer;" );
-        addStyleRule( "#pm5544StoreContainer a.remove:hover", "background:#8e1111; color:#fff;" );
+        addStyleRule( "#pm5544RestoreContainer", "text-align:left; position:fixed; z-index:9999999; width:500px; background:#fff; border:1px solid #eee; border-radius:5px; padding:5px; left:50%; top:10%; font:normal 12px/30px Verdana,sans-serif; margin-left:-250px; min-height:60px; max-height:80%; overflow-y:auto; box-shadow:2px 3px 10px rgba(0,0,0,.4);" );
+        addStyleRule( "#pm5544RestoreContainer a", "text-align:left; color:#000; display:inline-block; line-height:20px; padding:0 5px; width:445px; cursor:pointer; border:1px solid #e7e7e7; border-radius:5px; text-decoration:none;");
+        addStyleRule( "#pm5544RestoreContainer a.select:hover", "background:#e7e7e7;" );
+        addStyleRule( "#pm5544RestoreContainer a.select:hover:before", "content:'restore form with: ';" );
+        addStyleRule( "#pm5544RestoreContainer a.remove", "text-align:center; color:#8e1111; font-weight:bold; border:1px solid #8e1111; line-height:18px; margin-left:5px; width:19px; padding:0; cursor:pointer;" );
+        addStyleRule( "#pm5544RestoreContainer a.remove:hover", "background:#8e1111; color:#fff;" );
 
         addHandler( background, "click", removeElements );
         background.id = "pm5544RestoreBackground";
